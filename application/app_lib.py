def99a43c0f27fae55d92cf08010db818f1d55c6
Marton Neher <neher.marton@gmail.com>
2026-01-30 07:35:03 -0800

    Add applicaiton

---
+++ application/app_lib.py
@@ -0,0 +1,3 @@
+import proto.empty.empty_pb2 as pb2_types
+
+empty_message = pb2_types.Empty()
---
+++ application/main.py
@@ -0,0 +1,6 @@
+import click
+
+from application.app_lib import empty_message
+
+if __name__ == "__main__":
+  click.echo(f"Created empty message: {empty_message}")
